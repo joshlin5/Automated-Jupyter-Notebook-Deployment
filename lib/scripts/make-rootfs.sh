97135744d4f47ad2f378dbfa8fa146cb786f86f8
joshlin5 <linjos6750@hgmail.com>
2022-02-16 16:02:09 -0500

    stuck on ubuntu login

--- lib/scripts/make-rootfs.sh
+++ lib/scripts/make-rootfs.sh
@@ -9,23 +9,24 @@ set -e
 echo "Running rootfs build script"
 mkdir -p rootfs1
 dd if=/dev/zero of=rootfs seek=10240 bs=1M count=50
-mkfs.ext4 disk.img
-#mkdir -p /rootfs1/proc
-mount -o offset=0 disk.img /rootfs1
-#mount --bind /proc /rootfs/proc
+mkfs.ext4 -L cloudimg-rootfs rootfs
+mount -o offset=0 rootfs /rootfs1
 wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-arm64-root.tar.xz
 tar -xf focal-server-cloudimg-arm64-root.tar.xz -C /rootfs1
 chroot rootfs1 rm /etc/resolv.conf
 chroot rootfs1 bash -c "echo 'nameserver 8.8.8.8' | tee /etc/resolv.conf"
 chroot rootfs1 apt update
 chroot rootfs1 apt-get update
-chroot rootfs1 apt-get install python3.6
-chroot rootfs1 apt install -y python3-pip
-chroot rootfs1 apt install python3-venv python3-pip
-chroot rootfs1 pip install notebook
+chroot rootfs1 apt-get install python3
+chroot rootfs1 apt-get install -y python3-pip
+chroot rootfs1 apt-get install python3-notebook -y
+#chroot rootfs1 pip3 install notebook
+chroot rootfs1 pip3 install --no-input --exists-action=i pandas
+chroot rootfs1 pip3 install --no-input --exists-action=i seaborn
+chroot rootfs1 bash -c "groupadd -r ubuntu && useradd -m -r -g ubuntu ubuntu -s /bin/bash"
+chroot rootfs1 bash -c "echo 'ubuntu ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
+chroot rootfs1 bash -c "echo 'ubuntu:ubuntu' | chpasswd"
 wget https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv -P /rootfs1/data
 FK_MACHINE=none chroot rootfs1 apt install linux-virtual -y
-#unmount rootfs/proc
 umount rootfs1
-#cp rootfs1/boot/initrd.img rootfs/boot/vmlinuz scripts
 cp rootfs scripts
--- lib/scripts/package-iso.sh
+++ lib/scripts/package-iso.sh
@@ -10,8 +10,10 @@ mount -o offset=0 scripts/rootfs /rootfs1
 mkdir -p /iso/isolinux
 mkdir -p /iso/boot
 cp rootfs1/boot/initrd.img rootfs1/boot/vmlinuz /iso/boot
-cp rootfs1/boot/initrd.img rootfs1/boot/vmlinuz scripts
 cp /syslinux/isolinux.bin /syslinux/isolinux.cfg /syslinux/ldlinux.c32 /iso/isolinux/
 mkisofs -o jn.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J iso
-umount rootfs
-cp jn.iso syslinux
+cp rootfs1/boot/initrd.img rootfs1/boot/vmlinuz scripts
+mv scripts/initrd.img scripts/initrd
+mv scripts/vmlinuz scripts/vmlinuz.gz; gzip -d scripts/vmlinuz.gz
+umount rootfs1
+cp jn.iso scripts
